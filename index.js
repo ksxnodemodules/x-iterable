be2f1d7f778dea8d032647eb60d31861903d53a9
KSXGitHub <hvksmr1996@gmail.com>
2015-12-27 20:39:40 +0700

    resoft: alphabet :abc:

--- index.js
+++ index.js
@@ -4,8 +4,8 @@
 
 	module.exports = {
 		DeepIterable: require('./deep-iterable.js'),
-		ProductIterable: require('./product-iterable.js'),
 		ParallelIterable: require('./parallel-iterable.js'),
+		ProductIterable: require('./product-iterable.js'),
 		createClass: require('./create-class.js'),
 		root: require('./root.js'),
 		utils: require('./utils')
