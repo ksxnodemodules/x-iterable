437bff224a5f6e713591682f5bf267baca6e326f
KSXGitHub <hvksmr1996@gmail.com>
2015-12-27 21:07:14 +0700

    update utils/index.js

--- utils/index.js
+++ utils/index.js
@@ -1,5 +1,6 @@
 
 module.exports = {
 	isIterable: require('./is-iterable.js'),
+	parallelConstructor: require('./parallel-constructor.js'),
 	recursiveConstructor: require('./recursive-constructor.js')
 };
