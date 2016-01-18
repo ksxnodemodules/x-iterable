06a7828f35873927b1650445ad2ceaea3d1e9b13
KSXGitHub <hvksmr1996@gmail.com>
2016-01-19 00:39:36 +0700

    complete spread-iterable

---
+++ spread-iterable.js
@@ -0,0 +1,39 @@
+
+((module) => {
+	'use strict';
+
+	var createClass = require('./create-class.js');
+	var Root = require('./root.js').class;
+
+	var _key_iterator = Symbol.iterator;
+
+	class SpreadIterable extends Root {
+
+		constructor(iterable) {
+			this.iterable = iterable;
+		}
+
+		* [_key_iterator]() {
+			for (let element of this.iterable) {
+				yield * element;
+			}
+		}
+
+		static many(...args) {
+			return require('./concat-iterable.js')
+				.create(...args.map((iterable) => this.create(iterable)));
+		}
+
+		static times(...args) {
+			return SpreadIterable.deep(...args);
+		}
+
+		static deep(iterable, level) {
+			return this.create(level ? this.deep(iterable, level - 1) : iterable);
+		}
+
+	}
+
+	module.exports = createClass(SpreadIterable);
+
+})(module);
